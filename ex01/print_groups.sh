d268adf33c9f7f5d9152eab8ffa36ea7abde2e33
messouza <mesailde@ufscar.br>
2022-04-17 21:37:18 +0200

    print_groups

--- ex01/print_groups.sh
+++ ex01/print_groups.sh
@@ -1,6 +1,7 @@
+#!/bin/bash
 # sed é um editor de texto via linha de comando
-# a opção -z do sed vai executar todos os comandos na mesma entrada
+# a opção -z do sed vai executar todos os comandos na entrada inteira de uma única vez
 #s/ é utilizado para substituir o espaço / / pela /,/ de forma global = g
-# o ponto e vírgula separa a próxima substituição -> /\n/ substituir a quebr de linha por nada.
+# o ponto e vírgula separa a próxima substituição -> /\n/ substituir a quebra de linha por nada.
 
 groups $FT_USER | sed -z 's/ /,/g;s/\n//'
