fd850a8a69f2960b8d80562e9b407e638d71e749
messouza <mesailde@ufscar.br>
2022-04-17 22:04:13 +0200

    find_sh

--- ex02/find_sh.sh
+++ ex02/find_sh.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # o comando basename remove o 'sh' e o nome dos diretórios
 #o exec executa um comando pra cada objeto encontrado substituindo o {} pelo nome do arquivo
+#o ';' sinaliza o final do comando
 
 find -type f -name '*.sh' -exec basename {} .sh ';'
