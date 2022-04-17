7c6a1bbefd5b2a0a94c5f672c9c2af1270987ec2
messouza <mesailde@ufscar.br>
2022-04-17 22:32:06 +0200

    MAC.sh

--- ex04/MAC.sh
+++ ex04/MAC.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-ifconfig 
+#! /bin/sh
+# awk é uma linguagem de programação. 
+# Neste caso, o comando pega todas as linhas que contem "ether" no primeiro campo (considerando que os campos são separados por espaços)
+# e executa o print, que mostra apenas o segundo, terceiro, quarto e quinto campos, separados pela quantidade de espaços solicitada no enunciado.
+ifconfig | awk '$1=="ether"{print $2 "  " $3 " " $4 "  " $5}'
